d9accea5e7ea6b3cd87cb2166f2ae4654359a952
Frédéric Menou <frederic.menou@capitainetrain.com>
2016-06-11 14:03:47 +0200

    Ignore unknown headers

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,11 @@
 module Lib
     ( Match(..)
+    , Header(..)
+    , ParseError
     , parseFile
     ) where
+
+import Data.Maybe             (catMaybes)
 
 import Text.Parsec
 import Text.Parsec.ByteString (Parser, parseFromFile)
@@ -12,24 +16,40 @@ parseFile file = parseFromFile match file
 data Match = Match { matchHeaders :: [Header]
                    } deriving (Show)
 
-data Header = Header HeaderName String deriving (Show)
+data Header = Event String
+            | Site String
+            | Date String
+            | White String
+            | Black String
+            | Result String
+            | WhiteElo Int
+            | BlackElo Int
+            | PlyCount String
+            | Variant String
+            | TimeControl String
+            | ECO String
+            | Opening String
+            | Termination String
+            | Annotator String
+              deriving (Show)
 
-data HeaderName = Event
-                | Site
-                | Date
-                | White
-                | Black
-                | Result
-                | WhiteElo
-                | BlackElo
-                | PlyCount
-                | Variant
-                | TimeControl
-                | ECO
-                | Opening
-                | Termination
-                | Annotator
-                  deriving (Show, Read)
+parseHeader :: String -> String -> Maybe Header
+parseHeader "Event" = Just . Event
+parseHeader "Site" = Just . Site
+parseHeader "Date" = Just . Date
+parseHeader "White" = Just . White
+parseHeader "Black" = Just . Black
+parseHeader "Result" = Just . Result
+parseHeader "WhiteElo" = Just . WhiteElo . read
+parseHeader "BlackElo" = Just . BlackElo . read
+parseHeader "PlyCount" = Just . PlyCount
+parseHeader "Variant" = Just . Variant
+parseHeader "TimeControl" = Just . TimeControl
+parseHeader "ECO" = Just . ECO
+parseHeader "Opening" = Just . Opening
+parseHeader "Termination" = Just . Termination
+parseHeader "Annotator" = Just . Annotator
+parseHeader _ = const Nothing
 
 match :: Parser Match
 match = Match <$> headers <* body
@@ -38,20 +58,20 @@ body :: Parser [Char]
 body = many anyChar <* eof
 
 headers :: Parser [Header]
-headers = many header <* newline
+headers = catMaybes <$> many header <* newline
 
-header :: Parser Header
+header :: Parser (Maybe Header)
 header = char '[' *> headerContent <* char ']' <* newline
 
-headerContent :: Parser Header
+headerContent :: Parser (Maybe Header)
 headerContent = do
   name  <- headerName
   _     <- char ' '
   value <- headerValue
-  return $ Header name value
+  return $ parseHeader name value
 
-headerName :: Parser HeaderName
-headerName = read <$> many (noneOf " ")
+headerName :: Parser String
+headerName = many (noneOf " ")
 
 headerValue :: Parser String
 headerValue = char '"' *> many (noneOf "\"") <* char '"'
