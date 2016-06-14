28c543cf1b3b5c16d409534108d6187365c3cc75
Frédéric Menou <frederic.menou@capitainetrain.com>
2016-06-14 22:07:04 +0200

    Show PGN content

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,8 @@ module Main where
 
 import Lib
 
+import Control.Monad (unless)
+import Data.List (intersperse)
 import System.Environment (getArgs)
 import System.Exit (ExitCode(..), exitSuccess, exitWith)
 
@@ -10,12 +12,15 @@ main = getArgs >>= dispatch
 
 dispatch :: [String] -> IO ()
 dispatch ("check" : files) = mapM checkFile files >>= exit
-dispatch ("show"  : files) = mapM_ showFile files
+dispatch ("show"  : files) = sequence_ $ intersperse delimiter $ map showFile files
 dispatch ("help"  : _    ) = help
 dispatch ("-h"    : _    ) = help
 dispatch ("--help": _    ) = help
 dispatch (unknown : _    ) = putStrLn $ "Unknown action " ++ unknown
 dispatch []                 = help
+
+delimiter :: IO ()
+delimiter = putStrLn ""
 
 exit :: [Bool] -> IO ()
 exit checks =
@@ -43,4 +48,70 @@ checkFile file = do
           indent = unlines . map ("  "++) . lines
 
 showFile :: String -> IO ()
-showFile f = parseFile f >>= print
+showFile f = do
+  r <- parseFile f
+  case r of
+    Left  e -> print e
+    Right m -> printMatch m
+
+printMatch :: Match -> IO ()
+printMatch m = do
+  putStrLn "Headers:"
+  mapM_ (indented 1) $ matchHeaders m
+  putStrLn ""
+  putStrLn "Moves:"
+  mapM_ (\mv -> putStr "  " >> printMove mv) $ matchMoves m
+
+printMove :: Move -> IO ()
+printMove (Move number white black) = do
+  printMoveNumber number
+  printPly white
+  putStr " "
+  printPly black
+  putStrLn ""
+printMove (FinalMove number white black result) = do
+  printMoveNumber number
+  printMaybePly white
+  putStr " "
+  printMaybePly black
+  putStrLn ""
+  putStr " => "
+  print result
+
+printMoveNumber :: Int -> IO ()
+printMoveNumber number = putStr $ lpad 4 (show number ++ ". ")
+
+printMaybePly :: Maybe Ply -> IO ()
+printMaybePly = printMaybe printPly
+
+printPly :: Ply -> IO ()
+printPly (Ply m) = putStr (lpad 11 m)
+printPly (AnnotatedPly m g cs) = do
+  putStr (lpad 11 m)
+  printMaybeGlyph g
+  unless (null cs) $ do
+    putStr " « "
+    putStr $ unwords cs
+    putStr " »"
+
+printMaybeGlyph :: Maybe Glyph -> IO ()
+printMaybeGlyph = printMaybe printGlyph
+
+printMaybe :: (a -> IO ()) -> Maybe a -> IO ()
+printMaybe action (Just v) = action v
+printMaybe _      Nothing  = return ()
+
+printGlyph :: Glyph -> IO ()
+printGlyph (Glyph v) = putStr (" $" ++ show v)
+
+indented :: Show a => Int -> a -> IO ()
+indented width o = do
+  putStr $ take (width*2) $ repeat ' '
+  print o
+
+lpad :: Int -> String -> String
+lpad l s | l < 0     = error "padding size must be >= 0"
+         | otherwise = pad ++ s'
+  where l' = l - length s'
+        s' = take l s
+        pad = replicate l' ' '
