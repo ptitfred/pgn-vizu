00c6004bea302b4b50e1cd72326a1006b040c823
Frédéric Menou <frederic.menou@capitainetrain.com>
2016-06-14 23:06:55 +0200

    PGN proper module

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import PGN
 
 import Control.Monad (unless)
 import Data.List (intersperse)
--- src/PGN.hs
+++ src/PGN.hs
@@ -1,4 +1,4 @@
-module Lib
+module PGN
     ( Match(..)
     , Move(..)
     , Ply(..)
